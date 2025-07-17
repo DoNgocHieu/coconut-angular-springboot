a030f525487dda3bab8bd44cc1f8d658b7768166
Nguyen Tien Dat <datto2442004@gmail.com>
2025-07-17 15:57:05 +0700

    UPDATE DATA

--- start-backend.bat
+++ start-backend.bat
@@ -1,5 +1,5 @@
 @echo off
 echo 🥥 Starting Coconut Music Backend...
-cd /d "d:\works\coding\coconut-angular-springboot\backend"
+cd /d "c:\works\coconut-angular-springboot\backend"
 .\mvnw.cmd spring-boot:run
 pause
